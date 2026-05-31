d1c06ef41a8ac28e5189ecc75369f0b7a4ff7d87
shanswaraj18-alt <shanswaraj18@gmail.com>
2026-05-31 13:25:35 +0530

    DAY3

---
+++ DAY2/DAY3/QUESTION9.java
@@ -0,0 +1,24 @@
+//WAP to check whether the no. is prime
+package DAY2.DAY3;
+import java.util.Scanner;
+public class QUESTION9 {
+    public static void main(String[] args) {
+    Scanner Sc =new Scanner(System.in);
+    System.out.println("enter number to check prime or not:");
+    int num = Sc.nextInt();
+    boolean IsPrime = true;
+    if(num<=1){
+        IsPrime = false;
+    }
+    for(int i=2;i<=Math.sqrt(num);i++){
+     if(num%i!=0){
+        IsPrime = true;
+     }
+     else{
+        IsPrime=false;
+     }
+    }
+    System.out.println(IsPrime);
+}
+    
+}
