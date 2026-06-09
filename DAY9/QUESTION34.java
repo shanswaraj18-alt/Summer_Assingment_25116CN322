73ba5146c30c8f6a935227d8ac7b40171fc8681d
shanswaraj18-alt <shanswaraj18@gmail.com>
2026-06-09 11:18:26 +0530

    DAY9

---
+++ DAY9/QUESTION34.java
@@ -0,0 +1,12 @@
+package DAY9;
+public class QUESTION34 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=6-i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ DAY9/QUESTION35.java
@@ -0,0 +1,13 @@
+package DAY9;
+public class QUESTION35 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                int a =64+i;
+                char d =(char)a;
+                System.out.print(d);
+            }
+            System.out.println();
+        }
+    }
+    }
